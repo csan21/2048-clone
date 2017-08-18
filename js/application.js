a5035d2f7b4271775aa4133275f7b7da12c0d288
csan21 <chrisswho@gmail.com>
2017-08-18 15:14:01 -0500

    Space

--- js/application.js
+++ js/application.js
@@ -1,14 +1,14 @@
 $(document).ready(function() {
   game = new Game();
 
-  function renderBoard(){
-    arrayOfRows = $("table#256-table tr");
-    for(var r = 0; r < arrayOfRows.length; r++ ){
-      for(var c = 0; c < arrayOfRows.length; c++){
+  function renderBoard() {
+    arrayOfRows = $('table#256-table tr');
+    for (var r = 0; r < arrayOfRows.length; r++) {
+      for (var c = 0; c < arrayOfRows.length; c++) {
         $($(arrayOfRows[r]).children()[c]).html(game.board[r][c]);
-        }
       }
     }
+  }
   // hide zeroes on that board
   game.hideZeros();
   // render the board in the beginning
@@ -16,39 +16,55 @@ $(document).ready(function() {
   // put the zeros on that board
   game.showZeros();
 
-  Mousetrap.bind('up', function(){
-   game.upKey();
-   // insert the number
-   game.insertNum();
-   // hide the zeroes
-   game.hideZeros();
-   // show the board
-   renderBoard();
-   // puts the zeroes back where they were
-   game.showZeros();
-  }, 'keyup');
+  Mousetrap.bind(
+    'up',
+    function() {
+      game.upKey();
+      // insert the number
+      game.insertNum();
+      // hide the zeroes
+      game.hideZeros();
+      // show the board
+      renderBoard();
+      // puts the zeroes back where they were
+      game.showZeros();
+    },
+    'keyup'
+  );
 
-  Mousetrap.bind('down', function(){
-    game.downKey();
-    game.insertNum();
-    game.hideZeros();
-    renderBoard();
-    game.showZeros();
-  }, 'keyup');
+  Mousetrap.bind(
+    'down',
+    function() {
+      game.downKey();
+      game.insertNum();
+      game.hideZeros();
+      renderBoard();
+      game.showZeros();
+    },
+    'keyup'
+  );
 
-  Mousetrap.bind('left', function(){
-    game.leftKey();
-    game.insertNum();
-    game.hideZeros();
-    renderBoard();
-    game.showZeros();
-  }, 'keyup');
+  Mousetrap.bind(
+    'left',
+    function() {
+      game.leftKey();
+      game.insertNum();
+      game.hideZeros();
+      renderBoard();
+      game.showZeros();
+    },
+    'keyup'
+  );
 
-  Mousetrap.bind('right', function(){
-    game.rightKey();
-    game.insertNum();
-    game.hideZeros();
-    renderBoard();
-    game.showZeros();
-  }, 'keyup');
+  Mousetrap.bind(
+    'right',
+    function() {
+      game.rightKey();
+      game.insertNum();
+      game.hideZeros();
+      renderBoard();
+      game.showZeros();
+    },
+    'keyup'
+  );
 });
--- js/game.js
+++ js/game.js
@@ -1,188 +1,198 @@
-function Game(){
- this.board = [ [0,0,0,0],
-                [2,0,0,0],
-                [0,0,0,2],
-                [0,0,0,0] ]
+function Game() {
+  this.board = [[0, 0, 0, 0], [2, 0, 0, 0], [0, 0, 0, 2], [0, 0, 0, 0]];
 }
 
-Game.prototype.hideZeros = function(){
-  var singleArray = this.board[0].concat(this.board[1]).concat(this.board[2]).concat(this.board[3]);
+Game.prototype.hideZeros = function() {
+  var singleArray = this.board[0]
+    .concat(this.board[1])
+    .concat(this.board[2])
+    .concat(this.board[3]);
 
-  for(var i = 0; i < singleArray.length; i++){
-    if (singleArray[i] === 0){
-      singleArray[i] = "";
+  for (var i = 0; i < singleArray.length; i++) {
+    if (singleArray[i] === 0) {
+      singleArray[i] = '';
     }
   }
   // turn the single array back into it's original state
   this.board = [];
-  for(var j = 0; j < singleArray.length; j += 4){
+  for (var j = 0; j < singleArray.length; j += 4) {
     var subArray = [];
-    var num      = (j + 4)
+    var num = j + 4;
 
-    for(var i = j; i < num; i++){
-      subArray.push(singleArray[i])
+    for (var i = j; i < num; i++) {
+      subArray.push(singleArray[i]);
     }
     this.board.push(subArray);
   }
-}
+};
 
-Game.prototype.showZeros = function(){
-  var singleArray = this.board[0].concat(this.board[1]).concat(this.board[2]).concat(this.board[3]);
+Game.prototype.showZeros = function() {
+  var singleArray = this.board[0]
+    .concat(this.board[1])
+    .concat(this.board[2])
+    .concat(this.board[3]);
 
-  for(var i = 0; i < singleArray.length; i++){
-    if (singleArray[i] === ""){
+  for (var i = 0; i < singleArray.length; i++) {
+    if (singleArray[i] === '') {
       singleArray[i] = 0;
     }
   }
   // turn the single array back into it's original state
   this.board = [];
-  for(var j = 0; j < singleArray.length; j += 4){
+  for (var j = 0; j < singleArray.length; j += 4) {
     var subArray = [];
-    var num      = (j + 4)
+    var num = j + 4;
 
-    for(var i = j; i < num; i++){
-      subArray.push(singleArray[i])
+    for (var i = j; i < num; i++) {
+      subArray.push(singleArray[i]);
     }
     this.board.push(subArray);
   }
-}
+};
 
-Game.prototype.insertNum = function(){
+Game.prototype.insertNum = function() {
   // turn the board into one single array
   // go through the single array and look for any 0's log their indexes
-  var singleArray    = this.board[0].concat(this.board[1]).concat(this.board[2]).concat(this.board[3]);
+  var singleArray = this.board[0]
+    .concat(this.board[1])
+    .concat(this.board[2])
+    .concat(this.board[3]);
   var arrayOfIndexes = [];
 
-  for(var i = 0; i < singleArray.length; i++){
-    if (singleArray[i] === 0){
+  for (var i = 0; i < singleArray.length; i++) {
+    if (singleArray[i] === 0) {
       arrayOfIndexes.push(i);
     }
   }
   // if there's any 0's...
-  if (arrayOfIndexes.length > 0 ) {
+  if (arrayOfIndexes.length > 0) {
     if (arrayOfIndexes.length > 1) {
-      var randomIndex = Math.floor((Math.random() * arrayOfIndexes.length));
+      var randomIndex = Math.floor(Math.random() * arrayOfIndexes.length);
       var actualIndex = arrayOfIndexes[randomIndex];
-      } else {
+    } else {
       var actualIndex = arrayOfIndexes[0];
     }
     singleArray[actualIndex] = 2;
   }
   // turn the single array back into it's original state
   this.board = [];
-  for(var j = 0; j < singleArray.length; j += 4){
+  for (var j = 0; j < singleArray.length; j += 4) {
     var subArray = [];
-    var num      = (j + 4);
+    var num = j + 4;
 
-    for(var i = j; i < num; i++){
+    for (var i = j; i < num; i++) {
       subArray.push(singleArray[i]);
     }
     this.board.push(subArray);
   }
-}
+};
 
-Game.prototype.shift = function(array){
-  for(var i = 0; i < array.length; i++){
-    if (array[i+1] === 0){
+Game.prototype.shift = function(array) {
+  for (var i = 0; i < array.length; i++) {
+    if (array[i + 1] === 0) {
       var temp = array[i];
-      array[i] = array[i+1];
-      array[i+1] = temp;
+      array[i] = array[i + 1];
+      array[i + 1] = temp;
     }
   }
   return array;
-}
+};
 
-Game.prototype.ultimateCompile = function(array){
-  for(var i = 0; i < array.length; i++){
-    if (array[i] === array[i+1] && array[i+1] === array[i+2] && array[i+2] === array[i+3]){
-      temp = array[i+1] + array[i+2];
+Game.prototype.ultimateCompile = function(array) {
+  for (var i = 0; i < array.length; i++) {
+    if (
+      array[i] === array[i + 1] &&
+      array[i + 1] === array[i + 2] &&
+      array[i + 2] === array[i + 3]
+    ) {
+      temp = array[i + 1] + array[i + 2];
       array[i] = 0;
-      array[i+2] = 0;
-      array[i+1] = temp;
-      array[i+3] = temp;
-    } else if (array[i] === array[i+1] && array[i+1] === array[i+2]){
-      temp = array[i+1] + array[i+2];
-      array[i]   = 0;
-      array[i+2] = temp;
+      array[i + 2] = 0;
+      array[i + 1] = temp;
+      array[i + 3] = temp;
+    } else if (array[i] === array[i + 1] && array[i + 1] === array[i + 2]) {
+      temp = array[i + 1] + array[i + 2];
+      array[i] = 0;
+      array[i + 2] = temp;
       i += 2;
-    } else if (array[i] === array[i+1]){
-      temp = array[i] + array[i+1];
+    } else if (array[i] === array[i + 1]) {
+      temp = array[i] + array[i + 1];
       array[i] = 0;
-      array[i+1] = temp;
+      array[i + 1] = temp;
       i += 1;
     }
   }
   return array;
-}
+};
 
-Game.prototype.rotateLeft = function(){
+Game.prototype.rotateLeft = function() {
   var rotatedBoard = [];
 
-  for(var c = this.board.length-1 ; c >= 0; c--){
+  for (var c = this.board.length - 1; c >= 0; c--) {
     var tempArray = [];
 
-    for(var r = 0; r < this.board.length; r++){
+    for (var r = 0; r < this.board.length; r++) {
       tempArray.push(this.board[r][c]);
     }
     rotatedBoard.push(tempArray);
   }
   this.board = rotatedBoard;
-}
+};
 
-Game.prototype.rotateRight = function(){
+Game.prototype.rotateRight = function() {
   var rotatedBoard = [];
 
-  for(var c = 0; c < this.board.length ; c++){
+  for (var c = 0; c < this.board.length; c++) {
     var tempArray = [];
 
-    for(var r = this.board.length-1; r >= 0; r--){
+    for (var r = this.board.length - 1; r >= 0; r--) {
       tempArray.push(this.board[r][c]);
     }
     rotatedBoard.push(tempArray);
   }
   this.board = rotatedBoard;
-}
+};
 // user presses down key, board rotate to the left, shift & compile, board rotate to the right
-Game.prototype.downKey = function(){
+Game.prototype.downKey = function() {
   this.rotateLeft();
-  for(var i = 0; i < this.board.length; i++){
-    temp = this.shift(this.board[i]);
-    temp = this.shift(temp);
-    temp = this.ultimateCompile(temp);
-    temp = this.shift(temp);
-    this.board[i]  = this.shift(temp);
-  }
-  this.rotateRight();
-}
-// user presses up key, board rotate to the right, shift & compile, board rotate to the left
-Game.prototype.upKey = function(){
-  this.rotateRight();
-  for(var i = 0; i < this.board.length; i++){
-    temp = this.shift(this.board[i]);
-    temp = this.shift(temp);
-    temp = this.ultimateCompile(temp);
-    temp = this.shift(temp); // <-- need to shift one more time...
-    this.board[i]  = this.shift(temp);
-  }
-  this.rotateLeft();
-}
-
-Game.prototype.leftKey = function(){
-  for(var i = 0; i < this.board.length; i++){
-    temp = this.shift(this.board[i].reverse());
-    temp = this.shift(temp);
-    temp = this.ultimateCompile(temp);
-    temp = this.shift(temp);
-    this.board[i] = this.shift(temp).reverse();
-  }
-}
-
-Game.prototype.rightKey = function(){
-  for(var i = 0; i < this.board.length; i++){
+  for (var i = 0; i < this.board.length; i++) {
     temp = this.shift(this.board[i]);
     temp = this.shift(temp);
     temp = this.ultimateCompile(temp);
     temp = this.shift(temp);
     this.board[i] = this.shift(temp);
   }
-}
+  this.rotateRight();
+};
+// user presses up key, board rotate to the right, shift & compile, board rotate to the left
+Game.prototype.upKey = function() {
+  this.rotateRight();
+  for (var i = 0; i < this.board.length; i++) {
+    temp = this.shift(this.board[i]);
+    temp = this.shift(temp);
+    temp = this.ultimateCompile(temp);
+    temp = this.shift(temp); // <-- need to shift one more time...
+    this.board[i] = this.shift(temp);
+  }
+  this.rotateLeft();
+};
+
+Game.prototype.leftKey = function() {
+  for (var i = 0; i < this.board.length; i++) {
+    temp = this.shift(this.board[i].reverse());
+    temp = this.shift(temp);
+    temp = this.ultimateCompile(temp);
+    temp = this.shift(temp);
+    this.board[i] = this.shift(temp).reverse();
+  }
+};
+
+Game.prototype.rightKey = function() {
+  for (var i = 0; i < this.board.length; i++) {
+    temp = this.shift(this.board[i]);
+    temp = this.shift(temp);
+    temp = this.ultimateCompile(temp);
+    temp = this.shift(temp);
+    this.board[i] = this.shift(temp);
+  }
+};
